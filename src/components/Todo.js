65ef2a769a01255cc2329145bc471b2f6c427af6
agrgic97 <grgic.antonio97@gmail.com>
2022-03-28 00:19:46 +0200

    added update and delete functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import { database } from '../firebase-config';
+import { setDoc, deleteDoc, doc } from 'firebase/firestore'; 
 
 export default function Todo(props) {
-    const updateTodo = (id) => {
-        props.SetTodos(todos => todos.map(todo => {
-            if(todo.id === id) todo.completed = !todo.completed
-            return todo
-        }))
+    const updateTodo = async (id) => {
+        const docRef = doc(database, "todos", id);
+        const payload = { task: props.Task ,completed: !props.Completed };
+        
+        await setDoc(docRef, payload);
+    }
+
+    const deleteTodo = async (id) => {
+        const docRef = doc(database, "todos", id);
+
+        await deleteDoc(docRef);
     }
 
     return(
-        <div className={`Todo ${ props.Completed ? 'completed' : ''}`} onDoubleClick={ () => { updateTodo(props.ID) } }>
+        <div className={`Todo ${ props.Completed ? 'completed' : ''}`} onDoubleClick={ () => updateTodo(props.ID) }>
             <div>
                 <h2 className='Task'>{ props.Task }</h2>
             </div>
-            <span onClick={ () => { props.SetTodos(todos => todos.filter(todo => todo.id !== props.ID))}} role="img" aria-label='x'>&#10060;</span>
+            <span onClick={ () => deleteTodo(props.ID) } role="img" aria-label='x'>&#10060;</span>
         </div>
     )
 }
